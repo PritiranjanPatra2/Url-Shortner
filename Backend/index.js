ab3b7c237c38ee7cc057d45447079f1dcc2f9cc1
Pritiranjan <patrapritiranjan11@gmail.com>
2024-09-06 13:55:30 +0530

    port

--- Backend/index.js
+++ Backend/index.js
@@ -4,13 +4,15 @@ import router from './routes/router.js';
 import mongoose from 'mongoose';
 
 const app = express();
+const port=5000;
+
 
 app.use(express.json());
 app.use(cors());    
 app.use(express.urlencoded({extended:false}));
 app.use('/',router)
 mongoose.connect('mongodb://127.0.0.1:27017/Urls').then(()=>{
-    app.listen(5000,()=>{
+    app.listen(port,()=>{
         console.log("Server is running on port 5000")
     })
 }).catch(()=>{
